9f8722c528e919f7aeec6a16790bce5f3ed37a72
Eric Lewis <eric.lewis@generalassemb.ly>
2018-09-20 16:49:36 -0400

    Add body-parser middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5678;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
