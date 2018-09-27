d84df6c03cbbb10864cd868db371fe4a452724a9
Scott T <scottt@ool-ad020371.dyn.optonline.net>
2018-09-27 14:20:19 -0400

    added register function

--- src/LoginOrRegistration/index.js
+++ src/LoginOrRegistration/index.js
@@ -6,21 +6,47 @@ constructor(props) {
   super(props)
 
   this.state = {
-     newUsername: "",
-     newPassword: "",
+    userEmail: "",
+    password: "",
   }
 }
+
+register = async () => {
+  const body = JSON.stringify({
+    userEmail: this.state.userEmail,
+    password: this.state.password
+  });
+  const addUser = await(await fetch('/api/register', {
+    method: "POST",
+    body: body,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })).json();
+
+  localStorage.setItem('user-jwt', addUser);
+  this.props.onLogIn();
+}
+
+onInputChange = evt => {
+  this.setState({
+    [evt.target.name]: evt.target.value
+  })
+}
+
+
 
 
   render() {
     return (
       <div className="form-container">
         <form>  
-            <label className="label">Username</label>
-            <input className="input" type="text" name="newUsername" onChange={this.props.handleChange}/>
+            <label className="label">Email</label>
+            <input className="input" type="text" name="userEmail" onChange={this.onInputChange}/>
             <label>Password</label>
-            <input className="input" type="text" name="newPassword" onChange={this.props.handleChange}/>
-            <input type="submit" value="Submit" />
+            <input className="input" type="text" name="password" onChange={this.onInputChange}/>
+            <button type="button" onClick={this.register}>Register</button>
+            <button type="button" onClick={this.logIn}>Log in</button>
         </form>
       </div>
     )
