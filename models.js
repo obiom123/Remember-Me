eb7994bc353e20dad0b449596707ebbc15c703a5
Sarah Chai <sarahchai93@gmail.com>
2018-09-27 11:03:29 -0400

    created models

--- models.js
+++ models.js
@@ -1,15 +1,33 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
-  database: 'some_app_name',
+  database: 'project-3-remember-me',
   dialect: 'postgres'
 });
 
 
 // Create models here
+const User = sequelize.define('user', {
+  username: { type: Sequelize.STRING, unique: true },
+  passwordDigest: Sequelize.STRING
+})
+
+const Contact = sequelize.define('contact', {
+  name: Sequelize.TEXT,
+  contactInfo: Sequelize.TEXT,
+  whereWeMet: Sequelize.TEXT,
+  importance: Sequelize.INTEGER(2),
+  conversation: Sequelize.TEXT,
+  linkedinFriends: Sequelize.BOOLEAN
+})
+
+User.hasMany(Contact);
+Contact.belongsTo(User);
 
 
 module.exports = {
   // Export models
+  User: User,
+  Contact: Contact,
   sequelize: sequelize
 };
