8f4695402fac8a8b5e4cddf0a8a6906d3bd83c51
Obi Onuora <obimarcus11@gmail.COM>
2018-09-27 11:54:37 -0400

    React Router

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import "./style.css";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import LoginOrRegistration from "../LoginOrRegistration";
+
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <Router>
+        <div className="App">
+        <h1> Hello World app page</h1>
+          <nav> 
+        <Link to="/LoginOrRegistration">Login</Link>
+          </nav>
+        <Route path="/LoginOrRegistration" exact component={LoginOrRegistration} />
+
+        </div>
+    </Router>
+    )
   }
 }
 
---
+++ src/LoginOrRegistration/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class LoginOrRegistration extends Component {
+  render() {
+    return (
+      <div className="LoginOrRegistration">
+      <h1>Login</h1>
+     /</div>
+
+    )
+  }
+}
