a00ebc2d13f9ff41113dc091ff9b59c79e8bf026
Obi Onuora <obimarcus11@gmail.COM>
2018-09-27 14:53:19 -0400

    contacts component rendered

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./style.css";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LoginOrRegistration from "../LoginOrRegistration";
+import Contacts from "../Contacts";
+
 
 
 class App extends Component {
@@ -19,9 +21,13 @@ class App extends Component {
         <div className="App">
           <nav> 
         <Link to="/LoginOrRegistration">Login</Link>
+        <br></br>
+        <Link to="/Contacts">Contacts</Link>
           </nav>
-          <br></br>
+          
         <Route path="/LoginOrRegistration" exact component={LoginOrRegistration} />
+        <Route path="/Contacts" exact component={Contacts} />
+
 
         </div>
     </Router>
---
+++ src/Contacts/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import "./style.css"
+
+
+
+
+
+export default class Contacts extends Component {
+
+render() {
+  return (
+    <div className="form-container">✚ Contacts
+      
+
+    </div>
+  )
+}
+}
