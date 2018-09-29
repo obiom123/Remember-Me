17a0a353090b7b513319b123d08fb3f554ae195c
Sarah Chai <sarahchai93@gmail.com>
2018-09-28 23:45:50 -0400

    renamed contacts component and created detail contact page

---
+++ src/DetailContactPage/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./style.css"
+
+import React, { Component } from 'react'
+
+export default class DetailContactPage extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
