d91c56968b3e2940052af5e6f7dc64e0551f9d2e
“Scott <scotttsai2011@gmail.com>
2018-10-02 01:53:35 -0400

    style

--- src/App/index.js
+++ src/App/index.js
@@ -33,7 +33,7 @@ class App extends Component {
     const validateEmail = emailRegex.test(String(this.state.userEmail).toLowerCase());
     const validatePassword = passwordRegex.test(String(this.state.password));
 
-    await this.setState({
+    this.setState({
       emailValid: validateEmail,
       passwordValid: validatePassword,
     })
